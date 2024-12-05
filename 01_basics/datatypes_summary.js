d3c9e20bcb42c8d5c84720c3eaf3a615b6ac8c76
gunjansaikia <gunjansaikia535@gmail.com>
2024-12-05 17:31:44 +0000

    datatypes_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -35,4 +35,30 @@ let myobj = {
  console.log(typeof id);
  
 
+ //+++++++++++++++++++++++++++++++++++++++++++++++
+
+ // stack(Primitive), Heap (Non-Primitive)
+
+ let myYoutubename = "hiteshchoudharydotcom"
+
+ let anothername = myYoutubename
+ anothername = "chaiaurcode"
+
+ console.log(myYoutubename);
+ console.log(anothername);
+
+ let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+ }
+
+ let userTwo = userOne
+ 
+ userTwo.email = "hitesh@google.com"
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
+ 
+
+
  
