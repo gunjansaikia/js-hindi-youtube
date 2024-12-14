6f2a32fa716c57f53a5de15d6966c86caa5c2623
gunjansaikia <gunjansaikia535@gmail.com>
2024-12-14 16:57:01 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,13 +47,40 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedin'));
+// console.log(tinderUser.hasOwnProperty('isLoggedin'));
+
+
+const course = {
+    coursename: "js in hindi ",
+    price: "999",
+    courseInstructor: "gungun"
+}
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//    " name": "gungun",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+
+]
+
+
 
 
 
