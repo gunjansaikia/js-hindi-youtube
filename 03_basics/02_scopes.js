6897d0a676997bc63b0f44614184636e14eb4b11
gunjansaikia <gunjansaikia535@gmail.com>
2024-12-20 10:49:27 +0000

    arrow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,61 @@
+// var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("inner: ", a);
+    
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "gungun"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+   
+    two()
+}
+
+
+// one()
+
+if (true) {
+ const username = "gungun"
+ if (username === "gungun") {
+    const website = "  youtube"
+    // console.log(username + website);
+    
+ }
+ // console.log(website);
+ 
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++++++ interesting ++++++++++++++
+
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+
+}
